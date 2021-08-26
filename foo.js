db6e0c6b9cadd33ecaffec712ce35f2f57d59a72
Christian Parker <christianparker512@gmail.com>
2021-08-26 11:09:52 -0500

    linking the html

--- foo.js
+++ foo.js
@@ -1,7 +1,20 @@
+//you'd use an interface if you'd like to do an interface.
+//need an equal sign if you use a type. You don't need one with an interface.
+var Grault = /** @class */ (function () {
+    function Grault(quuz, waldo) {
+        this.garply = quuz.quuz + " " + quuz.corge + " " + waldo;
+    }
+    Grault.prototype.getGarply = function () {
+        return this.garply;
+    };
+    return Grault;
+}());
 //metasyntactic variable names
 function foo(bar) {
-    return "Hello, " + bar;
+    return "Hello, " + bar.quuz + " " + bar.corge;
 }
 var baz = { quuz: "abc", corge: 123 };
+var fred = new Grault(baz, [1, 2, 3]);
 // console.log(foo("car"));
-console.log(foo(baz));
+console.log(fred.getGarply());
+document.body.innerHTML = fred.getGarply();
--- foo.ts
+++ foo.ts
@@ -1,3 +1,16 @@
+//you'd use an interface if you'd like to do an interface.
+//need an equal sign if you use a type. You don't need one with an interface.
+class Grault {
+    private garply: string
+
+    constructor(quuz: Quux, waldo: number[]){
+        this.garply = quuz.quuz + " " + quuz.corge + " " + waldo
+    }
+    public getGarply() {
+        return this.garply
+    }
+}
+
 interface Quux {
     quuz: string
     corge: number
@@ -5,9 +18,11 @@ interface Quux {
 
 //metasyntactic variable names
 function foo(bar: Quux){
-    return "Hello, " + bar;
+    return "Hello, " + bar.quuz + " " + bar.corge ;
 }
 let baz = {quuz: "abc", corge: 123};
+let fred: Grault = new Grault (baz, [1,2,3])
 
 // console.log(foo("car"));
-console.log(foo(baz));
+console.log(fred.getGarply());
+document.body.innerHTML = fred.getGarply()
